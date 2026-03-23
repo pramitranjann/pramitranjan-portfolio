f9344689249fa125a742bdce52d550710c98b2f9
Pramit Ranjan <pramit.ranjan1106@gmail.com>
2026-03-22 21:21:43 -0400

    feat: wire IntroAnimation into homepage via dynamic import

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,17 @@ import { MoreWork }         from '@/components/MoreWork'
 import { About }            from '@/components/About'
 import { Contact }          from '@/components/Contact'
 import { Footer }           from '@/components/Footer'
+import dynamic from 'next/dynamic'
+const IntroAnimation = dynamic(
+  () => import('@/components/IntroAnimation').then(m => m.IntroAnimation),
+  { ssr: false }
+)
 
 export default function HomePage() {
   return (
     <>
       <Nav />
+      <IntroAnimation />
       <HeroCarousel />
       <main style={{ paddingTop: '57px' }}>
         <SelectedWork />
