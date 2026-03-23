28a65851f9b7bd2996bbc7ec4575cc84963e5c77
Pramit Ranjan <pramit.ranjan1106@gmail.com>
2026-03-23 10:59:59 -0400

    fix: simplify colophon text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,11 +5,7 @@ export function Footer() {
       style={{ borderTop: '1px solid var(--color-cardborder)', padding: '12px 24px' }}
     >
       <span style={{ fontSize: 'var(--text-meta)', letterSpacing: '0.1em', color: 'var(--color-label)', lineHeight: 1.8 }}>
-        Set in{' '}
-        <span style={{ color: 'var(--color-body)' }}>DM Serif Display</span>
-        {' '}and{' '}
-        <span style={{ color: 'var(--color-body)' }}>DM Mono</span>
-        . Built and designed between meals and gym sets. My mum thinks it looks nice. © 2026 PR_
+        Designed between meals and gym sets. My mum thinks it looks nice. © 2026 PR_
       </span>
     </footer>
   )
